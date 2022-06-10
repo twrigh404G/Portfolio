f88cd2ef53d6ebef6f6f4191430d4c4ced906873
Terrell <wrightterrell049@gmail.com>
2022-06-10 11:50:09 -0400

    Footer NavBar Components started

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,28 @@
 
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/react';
+import { FC } from 'react';
 
+const footerStyles = css({
+    color: 'red',
+  },
+});
 
-const Footer = () => {
-    return (
-        <Container>
-
-        </Container>
-    );
-}
+const Footer: FC = () => {
+  return (<footer>
+      <Grid container>
+        <Grid item xs={12}>
+            <Paper css={footerStyles}>Hello</Paper>
+        </Grid>
+      </Grid>
+      </footer>)
+};
 
 export default Footer;
+
+
+
